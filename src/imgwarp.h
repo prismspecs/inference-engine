d54b23612b1cc0188b9e8232f38536bcf1e27223
Grayson Earle <graysonearle@gmail.com>
2021-08-26 01:07:36 +0200

    tinkering

---
+++ src/imgwarp.h
@@ -0,0 +1,22 @@
+#ifndef _IMGWARP 
+#define _IMGWARP 
+#include "ofMain.h" 
+
+class ImageWarp {
+
+    public: 
+
+    void setup(ofImage image);  
+    void update(); 
+    void draw(); 
+
+    // variables
+    ofImage img;
+    float alpha = 255;
+    float alphaDrain = 10;
+    float scale = 1;
+    float scaleSpeed = .01;
+
+    ImageWarp(); 
+}; 
+#endif
